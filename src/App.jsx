86149b95cb86ede129468a672084ae2e67092a3d
Yaroslav-Borisov <yarpolar@gmail.com>
2023-12-07 22:23:37 +0300

    Добавит роутинг

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import { Header } from './components/Header/Header';
 import { Layout } from './pages/layout/layout';
 import { sneakers } from '../mock-data/mock-data';
 import { Drawer } from './components/Drawer/Drawer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
 	return (
-		<div className="page-wrapper">
-			<Header/>
-			<Layout cards={sneakers} title={'Все кроссовки'} hasSearch={true}/>
-			<Drawer/>
-		</div>
+		<BrowserRouter>
+			<div className="page-wrapper">
+				<Header/>
+				<Routes>
+					<Route
+						path='/'
+						element={<Layout cards={sneakers} title={'Все кроссовки'} hasSearch={true}/>}
+					/>
+					<Route
+						path='/favorites'
+						element={<Layout cards={sneakers} title={'Избранные кроссовки'} hasSearch={false}/>}
+					/>
+					<Route
+						path='/orders'
+						element={<Layout cards={sneakers} title={'Мои заказы'} hasSearch={false}/>}
+					/>
+				</Routes>
+				<Drawer/>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,24 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
 	return (
 		<header className="page-header">
-			<a className="page-header__logo logo" href="#">
+			<Link className="page-header__logo logo" to="/">
 				<img className="logo__image" src="img/logo.png" width="40" height="40" />
 				<span className="logo__title">Sneakers-Shop</span>
 				<span className="logo__desc">Магазин брендовых кроссовок</span>
-			</a>
+			</Link>
 			<nav className="page-header__nav main-nav">
 				<a className="main-nav__link" href="#">
 					<img className="main-nav__icon" src="img/cart.svg" width="20" height="19" />
 					<span className="main-nav__text">1205 руб.</span>
 				</a>
-				<a className="main-nav__link" href="#">
+				<Link className="main-nav__link" to="/favorites">
 					<img className="main-nav__icon" src="img/fav.svg" width="20" height="19" />
-				</a>
-				<a className="main-nav__link" href="#">
+				</Link>
+				<Link className="main-nav__link" to="/orders">
 					<img className="main-nav__icon" src="img/user.svg" width="20" height="20" />
-				</a>
+				</Link>
 			</nav>
 		</header>
 	);
