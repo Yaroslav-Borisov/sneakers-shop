10c706a73af786fb2083b6bead90f2da8f62bbf3
Yaroslav-Borisov <yarpolar@gmail.com>
2023-12-07 22:59:36 +0300

    Добавит работу поиска

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,41 @@ import { Layout } from './pages/layout/layout';
 import { sneakers } from '../mock-data/mock-data';
 import { Drawer } from './components/Drawer/Drawer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppContext } from './context/app.context';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [updatedCards, setUpdatedCards] = useState(sneakers);
+	const [filteredCards, setFilteredCards] = useState(updatedCards);
+	const [searchInputText, setSearchInputText] = useState('');
+
+	useEffect(() => {
+		setFilteredCards(updatedCards.filter(card => card.title.toLowerCase().includes(searchInputText.toLowerCase())));
+	}, [searchInputText, updatedCards]);
 
 	return (
-		<BrowserRouter>
-			<div className="page-wrapper">
-				<Header/>
-				<Routes>
-					<Route
-						path='/'
-						element={<Layout cards={sneakers} title={'Все кроссовки'} hasSearch={true}/>}
-					/>
-					<Route
-						path='/favorites'
-						element={<Layout cards={sneakers} title={'Избранные кроссовки'} hasSearch={false}/>}
-					/>
-					<Route
-						path='/orders'
-						element={<Layout cards={sneakers} title={'Мои заказы'} hasSearch={false}/>}
-					/>
-				</Routes>
-				<Drawer/>
-			</div>
-		</BrowserRouter>
+		<AppContext.Provider value={{searchInputText, setSearchInputText, updatedCards, setUpdatedCards}}>
+			<BrowserRouter>
+				<div className="page-wrapper">
+					<Header/>
+					<Routes>
+						<Route
+							path='/'
+							element={<Layout cards={filteredCards} title={'Все кроссовки'} hasSearch={true}/>}
+						/>
+						<Route
+							path='/favorites'
+							element={<Layout cards={updatedCards} title={'Избранные кроссовки'} hasSearch={false}/>}
+						/>
+						<Route
+							path='/orders'
+							element={<Layout cards={updatedCards} title={'Мои заказы'} hasSearch={false}/>}
+						/>
+					</Routes>
+					<Drawer/>
+				</div>
+			</BrowserRouter>
+		</AppContext.Provider>
 	);
 }
 
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context/app.context';
+
 export const Search = () => {
+	const { setSearchInputText, searchInputText } = useContext(AppContext);
+
+	const inputChangeHandler = (evt) => {
+		setSearchInputText(evt.target.value.trim());
+	};
 
 	return (
+
 		<div className="page-content__search search">
 			<img className="search__icon" src="img/search.svg" width="14" height="`4" />
-			<input className="search__field" type="search" placeholder="Поиск"/>
+			<input className="search__field" type="search" placeholder="Поиск" value={searchInputText} onChange={inputChangeHandler}/>
 		</div>
 	);
 };
